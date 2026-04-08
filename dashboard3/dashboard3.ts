c1ee962d3096827f16911c3b8f75186044672ee7
NoviaEkaS <154037857+NoviaEkaS@users.noreply.github.com>
2026-04-08 14:58:54 +0700

    Add files via upload

---
+++ dashboard3/dashboard3.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Header } from '../header/header';
+import { Sidebar } from '../sidebar/sidebar';
+import { Footer } from '../footer/footer';
+
+@Component({
+  selector: 'app-dashboard3',
+  standalone: true,
+  imports: [Header, Sidebar, Footer, RouterModule],
+  templateUrl: './dashboard3.html',
+  styleUrl: './dashboard3.css',
+})
+export class Dashboard3 {}
